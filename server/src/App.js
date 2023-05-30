1b3130a76458bca6ff14bd451d355f10e4b5aa76
Gina Vargas <gvargasp@hotmail.com>
2023-05-29 23:30:16 -0400

    problema de mayuscula desde App.js a app.js en el back end

--- server/src/App.js
+++ server/src/App.js
@@ -1,14 +1,3 @@
-// Front End
-/*
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './componentes/Navigation';
-import CreateNote from './componentes/CreateNote';
-import CreateUser from './componentes/CreateUser';
-import NotesList from './componentes/NotesList';
-*/
 //Back End 
 const express = require('express');
 const cors = require('cors');
@@ -23,21 +12,5 @@ app.use(express.json());
 // routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/notes', require('./routes/notes'))
-// frontend
-/*
-export default function App() {
-  return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" exact element={ <NotesList /> } />
-        <Route path="/edit/:id" element={ <CreateNote/> } />
-        <Route path="/create" element={ <CreateNote/> } />
-        <Route path="/user" element={ <CreateUser/> } />
-      </Routes>
-    </Router>  
-  );
-}
-*/
 
-module.exports = App;
+module.exports = app;
